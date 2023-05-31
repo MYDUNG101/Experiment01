13c5346cc2acd15614e06003e5c20bb8367f98ff
MYDUNG <mydunggg@gmail.com>
2023-06-01 01:40:16 +0200

    Update

--- data.js
+++ data.js
@@ -1,67 +1,33 @@
-// export var images = {
-// 	1: {
-// 		"image": "1_Response.png"
-// 	},
-// 	2: {
-// 		"image": "1_Condition.png"
-// 	},
-// 	3: {
-// 		"image": "1_Milestone.png"
-// 	},
-// 	4: {
-// 		"image": "1_Exclude.png"
-// 	},
-// 	5: {
-// 		"image": "1_Include.png"
-// 	},
-// 	6: {
-// 		"image": "2_Response.png"
-// 	},
-// 	7: {
-// 		"image": "2_Condition.png"
-// 	},
-// 	8: {
-// 		"image": "2_Milestone.png"
-// 	},
-// 	9: {
-// 		"image": "2_Exclude.png"
-// 	},
-// 	10: {
-// 		"image": "2_Include.png"
-// 	},
-
-// }
-
 export var images = {
 	1: {
-		"image": "test_include.png"
+		"image": "1_Response.png"
 	},
 	2: {
-		"image": "test_exclude.png"
+		"image": "1_Condition.png"
 	},
 	3: {
-		"image": "test_condition.png"
+		"image": "1_Milestone.png"
 	},
 	4: {
-		"image": "test_milestone.png"
+		"image": "1_Exclude.png"
 	},
 	5: {
-		"image": "test_response.png"
+		"image": "1_Include.png"
 	},
 	6: {
-		"image": "test_include.png"
+		"image": "2_Response.png"
 	},
 	7: {
-		"image": "test_exclude.png"
+		"image": "2_Condition.png"
 	},
 	8: {
-		"image": "test_condition.png"
+		"image": "2_Milestone.png"
 	},
 	9: {
-		"image": "test_milestone.png"
+		"image": "2_Exclude.png"
 	},
 	10: {
-		"image": "test_response.png"
+		"image": "2_Include.png"
 	},
 
 }
@@ -70,11 +36,11 @@ var imagesIndex = Object.keys(images);
 export var shuffledImages = imagesIndex.sort((a, b) => 0.5 - Math.random());
 
 export var answers = {
-	1: "If you \"Draw card\" then you <b><u>cannot</u></b> \"Eat cake\".",
-	2: "In order to \"Eat cake\" you <b><u>need</u></b> to \"Draw card\" at least once",
-	3: "If you \"Draw card\" then you are able to \"Eat cake\"; i.e., if you were not able to \"Eat cake\" before and \"Draw card\", then you <b><u>can</u></b> \"Eat cake\" now.",
-	4: "If you \"Draw card\" you <b><u>need</u></b> to at some point \"Eat cake\"; i.e., \"Eat cake\" is now <b><u>required</u></b>",
-	5: "You can \"Eat cake\" <b><u>unless</u></b> \"Draw card\" is required. If \"Draw card\" is required, then in order to \"Eat cake\" you need to \"Draw card\".",
+	1: "If you \"Draw card\" then you <b><u>cannot</u></b> \"Play black card\".",
+	2: "In order to \"Play black card\" you <b><u>need</u></b> to \"Draw card\" at least once",
+	3: "If you \"Draw card\" then you are able to \"Play black card\"; i.e., if you were unable to \"Play black card\" before, you can now do so after you \"Draw card\".",
+	4: "If you \"Draw card\" you <b><u>need</u></b> to at some point \"Play black card\"; i.e., \"Play black card\" is now <b><u>required</u></b>",
+	5: "You can \"Play black card\" <b><u>unless</u></b> \"Draw card\" is required. If \"Draw card\" is required, then in order to \"Play black card\" you need to \"Draw card\" first.",
 	6: "No description fits"
 }
 
@@ -85,46 +51,50 @@ export var pages = {
 	},
 	"about": {
 		"title": "ABOUT",
-		"text": "My name is Dung M. T. Trinh and I am a bachelor student at Technical University of Denmark (DTU). <br> <br> As part of my bachelor project, I am conducting a survey as one of my studies. The purpose of this survey is to investigate the understanding of the <b><i>visual representation of relations</i></b> with its intended meaning for Dynamic Condition Response (DCR) graphs. <br> <br>Before the survey you will be provided with an explanation and an example of the use of DCR relations. You will also be given instructions on how to complete the survey <br> <br><b>Please read the explanation, example of use and instructions thoroughly.</b>.",
+		"text": "My name is Dung M. T. Trinh and I am a bachelor student at Technical University of Denmark (DTU). <br> <br> As part of my bachelor project, I am conducting a survey as one of my studies. The purpose of this survey is to investigate the understanding of the <b><i>visual representation of relations</i></b> with its semantic meaning for Dynamic Condition Response (DCR) graphs. <br> <br>Before the survey you will be provided with an explanation and an example of the use of DCR relations. You will also be given instructions on how to complete the survey <br> <br><b>Please read the explanation and instructions thoroughly.</b>.",
 		"button": "CONTINUE"
 	},
-	// "instructions": {
-	// 	"title": "INSTRUCTIONS",
-	// 	"text": "Before the survey begins you will need to know about <b><i>events</i></b> and <b><i>relations</i></b>.<ul><li>An <b><i>event</i></b>, which is visualized by a rounded square, represents something that can happen.</li><li>A <b><i>relation</i></b> represents a relationship between two events.</li></ul>In a moment, you will be shown an image of a relation between two events (As the image below). Furthermore, you will be shown 6 different descriptions. Your task is to choose the description that you think most accurately describes the relation in the image. If no description fits, then click \"No description fits\".",
-	// 	"text_events":"To easier refer to an event, the descriptions will refer to one event as <b><i>A</i></b> and the other event as <b><i>B</i></b>. A description can therefore look like <i>\"If A ... then B...\"</i><br>The events shown in the image will not be referred to as A or B, but instead as two icons (spades and diamond) (i.e. the icons in the events has no meaning. They only indicate that the two events are different from each other).",
-	// 	"button": "CONTINUE"
-	// },
 	"dcr": {
 		"title": "Dynamic Condition Response (DCR)",
-		"text": "There are two types of nodes in DCR grpahs:<ul><li>An <b><i>event</i></b>, which is visualized by a rectangle, represents something that can happen.</li><li>A <b><i>relation</i></b> represents a relationship between two events. The relations in DCR are like constraints that you can apply to the events. </li></ul> In the image example below, the <b><i>events</i></b> are indicated as rectangles, where one event represents \"Drink water\" and the other event represents \"Eat food\". The <b><i>relation</i></b> is indicated by the continuous connection between the two events.",
+		"text_event": "There are two types of nodes in DCR grpahs:<ul><li>An <b><i>event</i></b>, which is visualized by a rectangle, represents something that can happen. E.g.: \"Draw yellow card\":</li></ul>",
 
-		"text_pre_markings": "An event can be in a state. The default state of an event is <i>not executed</i>, <i>not pending</i>, and <i>included</i> (see figure below). ",
+		"text_relation": "<ul><li>A <b><i>relation</i></b> represents a relationship between two events. The relations in DCR are like constraints that you can apply to the events. The relation is indicated by the continuous connection between the two events:</li></ul>",
 
-		"text_markings": "<b>Executed event</b>: An executed event is marked with a green check mark and indicates that the event has happened.<br> E.g. If the event is \"Drink water\", then by adding the green check mark, one has indicated that \"Drink water\" has happened. <br><br> <b>Excluded event</b>: An excluded event is marked with a dashed outline and indicates that the event cannot be executed.<br>E.g. If the event is \"Drink water\", then by having the dashed outline, one has indicated that \"Drink water\" cannot happen.<br><br> <b>Pending event</b>: A pending event is marked with a blue exclamation mark and indicates that the event must eventually happen and is therefore now required.<br>E.g. If the event is \"Drink water\", then by adding the blue exclamation mark, one has indicated that \"Drink water\" must eventually happen.", 
+		"text_pre_markings": "An event can be in a state. The default state of an event is <b><i>not executed</i></b>, <b><i>not pending</i></b>, and <b><i>included</i></b> (see figure below). ",
 
-		"text_next": "Following is an example of the use of DCR relations.",
+		"text_markings": "<b>Executed event</b>: An executed event is marked with a green check mark and indicates that the event has happened.<br> E.g. If the event is \"Draw yellow card\", then by adding the green check mark, one has indicated that \"Draw yellow card\" has happened. <br><br> <b>Excluded event</b>: An excluded event is marked with a dashed outline and indicates that the event cannot be executed.<br>E.g. If the event is \"Draw yellow card\", then by having the dashed outline, one has indicated that \"Draw yellow card\" cannot happen.<br><br> <b>Pending event</b>: A pending event is marked with a blue exclamation mark and indicates that the event <u>needs to happen at some point</u> and is therefore now <u>required</u>.<br>E.g. If the event is \"Draw yellow card\", then by adding the blue exclamation mark, one has indicated that \"Draw yellow card\" is <u>required</u> and <u>needs to happen at some point</u>.", 
+
+		"text_next": "<br><br>Following is the instructions for the survey. By continuing it is assumed that you are familiar with the presented concepts about DCR <b><u>(You can take a screenshot if needed)</u></b>.",
 		"button": "CONTINUE"
 	},
 	"example": {
 		"title": "EXAMPLE",
-		"text": "In the following you will be presented 5 descriptions. The descriptions explains a type of relation between two events: \"Draw card\" and \"Eat cake\".<br><br><br>" ,
+		"text": "In the following you will be presented 5 descriptions. The descriptions explains a type of relation between two events: \"Draw card\" and \"Play black card\".<br><br><br>" ,
 
-		"text_exclude": "<hr>If you \"Draw card\" then you <b><u>cannot</u></b> \"Eat cake\".",
-		
-		"text_condition": "<hr>In order to \"Eat cake\" you <b><u>need</u></b> to \"Draw card\" at least once",
 
-		"text_response": "<hr>If you \"Draw card\" you <b><u>need</u></b> to at some point \"Eat cake\"; i.e., \"Eat cake\" is now <b><u>required</u></b>",
-
-		"text_include": "<hr>If you \"Draw card\" then you are able to \"Eat cake\"; i.e., if you were not able to \"Eat cake\" before and \"Draw card\", then you <b><u>can</u></b> \"Eat cake\" now.",
-
-		"text_milestone": "<hr>You can \"Eat cake\" <b><u>unless</u></b> \"Draw card\" is required. If \"Draw card\" is required, then in order to \"Eat cake\" you need to \"Draw card\".<hr>",
 		"text_next": "<br><br> Following is the instructions for the survey.",
 		"button": "CONTINUE"
 	},
 	"instructions":{
 		"title": "INSTRUCTIONS",
-		// "text":"In a moment, you will be shown an image of a relation between two events (like the image above). Below the image, you will be shown 6 different descriptions similar to the ones in the example. Your task is to choose the description that you consider most accurately describes the relation shown in the image. If you consider that no description fits, then choose \"No description fits\". You may choose one description for multiple relations.<br><br>The descriptions are expressed in terms of concrete situations, but try to think of them in a more general sense.",
-		"text":"In a moment, you will be shown an image of one type of relation an event and three other events. The three other events may have markings (like the image above).  Below the image, you will be shown 6 different descriptions similar to the ones in the example. Each image will have a highlighted event (red) which is the event that is refered to in the descriptions. Your task is to choose the description that you consider most accurately describes the relation shown in the image. If you consider that no description fits, then choose \"No description fits\". You may choose one description for multiple relations.<br><br>The descriptions are expressed in terms of concrete situations, but try to think of them in a more general sense.",
+		"text":"In a moment, you will be shown an image with one type of relation and four events. Two of the events and the relation between them will be highlighted with red. (See example below)",
+
+		"text_descriptions": "Below the image, you will be shown 6 different descriptions (in no particular order). The text \"Draw card\" in the descriptions is a generic text that applies to all of the events that includes something about drawing a card. However, you should replace \"Draw card\" with the specific text on the highlighted event. For example, in the above image example \"Draw white card\" is highlighted which means that \"Draw card\" in the following descriptions should be replaced with \"Draw white card\".<br><br><b>The descriptions are as follows:</b>",
+
+		"text_exclude": "<hr><ul><li>If you \"Draw card\" then you <b><u>cannot</u></b> \"Play black card\".</li></ul>",
+		
+		"text_condition": "<hr><ul><li>In order to \"Play black card\" you <b><u>need</u></b> to \"Draw card\" at least once</li></ul>",
+
+		"text_response": "<hr><ul><li>If you \"Draw card\" you <b><u>need</u></b> to at some point \"Play black card\"; i.e., \"Play black card\" is now <b><u>required</u></b></li></ul>",
+
+		"text_include": "<hr><ul><li>If you \"Draw card\" then you are able to \"Play black card\"; i.e., if you were unable to \"Play black card\" before, you can now do so after you \"Draw card\".</li></ul>",
+
+		"text_milestone": "<hr><ul><li>You can \"Play black card\" <b><u>unless</u></b> \"Draw card\" is required. If \"Draw card\" is required, then in order to \"Play black card\" you need to \"Draw card\".</li></ul>",
+
+		"text_none": "<hr><ul><li>No description fits</li></ul><hr>",
+
+		"text_task": "<br><br>Your task is to choose the description that you consider most accurately describes the highlighted relation shown in the image. If you consider that no description fits, then choose \"No description fits\". You may choose one description for multiple relations. The survey does not test your performance (all answers are good answers), but help us identify whether the relations reflects their intended meaning.",
+
 		"button": "CONTINUE"
 	},
 	"instructions2":{
@@ -132,8 +102,8 @@ export var pages = {
 		"button": "START"
 	},
 	"questions":{
-		"text_task":"Which of the following descriptions most immediately and accurately describes the symbol for the relation in the image?<br><i>(\"Draw card\" is a general term used to generalize one of the three leftmost events)</i>",
-		"tooltip":"\"Events\": Something that can happen (The two rectangles). <br> <br> \"Relation\": The relationship and connection between the two events.",
+		"text_task":"Which of the following descriptions most immediately and accurately describes the symbol for the relation in the image?<br><i>(\"Draw card\" is a general term used to generalize the highlighted event that draws a colored card.)</i>",
+		"tooltip":"\"Events\": Something that can happen. <br> <br> \"Relation\": The relationship and connection between two events.",
 		"button": "Next"
 	},
 	"demographics":{
--- introduction.js
+++ introduction.js
@@ -33,14 +33,23 @@ export function displayDCR(){
     let title = general.createTitle(data.pages.dcr.title);
     container.appendChild(title);
 
-    let para = general.createParagraph(data.pages.dcr.text);
-    container.appendChild(para);
+    let paraEvent = general.createParagraph(data.pages.dcr.text_event);
+    container.appendChild(paraEvent);
 
     //Image
-    let img = document.createElement("img");
-    img.src = "images/example.png";
-    img.classList.add("question_image_example");
-    container.appendChild(img);
+    let imgEvent = document.createElement("img");
+    imgEvent.src = "images/event.png";
+    imgEvent.classList.add("question_image_example");
+    container.appendChild(imgEvent);
+
+    let paraRelation = general.createParagraph(data.pages.dcr.text_relation);
+    container.appendChild(paraRelation);
+
+    //Image
+    let imgRelation = document.createElement("img");
+    imgRelation.src = "images/event_relation.png";
+    imgRelation.classList.add("question_image_example");
+    container.appendChild(imgRelation);
 
     // Markings
     let paraPreMarkings = general.createParagraph(data.pages.dcr.text_pre_markings);
@@ -58,17 +67,55 @@ export function displayDCR(){
     container.appendChild(paraNext);
 
     let btn = general.createButton(data.pages.dcr.button, function() {
-        displayInstructionsExample();
+        displayInstructions();
     })
     container.appendChild(btn);
 }
 
-export function displayInstructionsExample(){
+// export function displayInstructionsExample(){
+//     container.innerHTML = "";
+//     let title = general.createTitle(data.pages.example.title);
+//     container.appendChild(title);
+
+//     let para = general.createParagraph(data.pages.example.text);
+//     container.appendChild(para);
+
+//     //Image
+//     let img = document.createElement("img");
+//     img.src = "images/example2.png";
+//     img.classList.add("question_image_example");
+//     container.appendChild(img);
+
+//     let para_exclude = general.createParagraph(data.pages.example.text_exclude);
+//     container.appendChild(para_exclude);
+
+//     let para_condition = general.createParagraph(data.pages.example.text_condition);
+//     container.appendChild(para_condition);
+
+//     let para_include = general.createParagraph(data.pages.example.text_include);
+//     container.appendChild(para_include);
+
+//     let para_response = general.createParagraph(data.pages.example.text_response);
+//     container.appendChild(para_response);
+
+//     let para_milestone = general.createParagraph(data.pages.example.text_milestone);
+//     container.appendChild(para_milestone);
+
+//     let para_next = general.createParagraph(data.pages.example.text_next);
+//     container.appendChild(para_next);
+
+//     let btn = general.createButton(data.pages.example.button, function() {
+//         displayInstructions();
+//     })
+//     container.appendChild(btn);
+// }
+
+export function displayInstructions(){
     container.innerHTML = "";
-    let title = general.createTitle(data.pages.example.title);
+    let title = general.createTitle(data.pages.instructions.title);
     container.appendChild(title);
 
-    let para = general.createParagraph(data.pages.example.text);
+    let para = general.createParagraph(data.pages.instructions.text);
     container.appendChild(para);
 
     //Image
@@ -77,45 +124,31 @@ export function displayInstructionsExample(){
     img.classList.add("question_image_example");
     container.appendChild(img);
 
-    let para_exclude = general.createParagraph(data.pages.example.text_exclude);
+    let paraDescriptions = general.createParagraph(data.pages.instructions.text_descriptions);
+    container.appendChild(paraDescriptions);
+
+    let para_exclude = general.createParagraph(data.pages.instructions.text_exclude);
     container.appendChild(para_exclude);
 
-    let para_condition = general.createParagraph(data.pages.example.text_condition);
+    let para_condition = general.createParagraph(data.pages.instructions.text_condition);
     container.appendChild(para_condition);
 
-    let para_include = general.createParagraph(data.pages.example.text_include);
+    let para_include = general.createParagraph(data.pages.instructions.text_include);
     container.appendChild(para_include);
 
-    let para_response = general.createParagraph(data.pages.example.text_response);
+    let para_response = general.createParagraph(data.pages.instructions.text_response);
     container.appendChild(para_response);
 
-    let para_milestone = general.createParagraph(data.pages.example.text_milestone);
+    let para_milestone = general.createParagraph(data.pages.instructions.text_milestone);
     container.appendChild(para_milestone);
 
-    let para_next = general.createParagraph(data.pages.example.text_next);
-    container.appendChild(para_next);
+    let para_none = general.createParagraph(data.pages.instructions.text_none);
+    container.appendChild(para_none);
 
-    let btn = general.createButton(data.pages.example.button, function() {
-        displayInstructions();
-    })
-    container.appendChild(btn);
-}
+    let paraTask = general.createParagraph(data.pages.instructions.text_task);
+    container.appendChild(paraTask);
 
-export function displayInstructions(){
-    container.innerHTML = "";
-    let title = general.createTitle(data.pages.instructions.title);
-    container.appendChild(title);
-
-    //Image
-    let img = document.createElement("img");
-    img.src = "images/example3.png";
-    img.classList.add("question_image_example");
-    container.appendChild(img);
-
-    let para = general.createParagraph(data.pages.instructions.text);
-    container.appendChild(para);
-
-    let btn = general.createButton(data.pages.example.button, function() {
+    let btn = general.createButton(data.pages.instructions.button, function() {
         displayInstructions2();
     })
     container.appendChild(btn);
